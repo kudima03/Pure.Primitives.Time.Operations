683bc9b2d4b1ba4a395d2447ee6aa725ac3fc3df
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-18 17:09:23 +0300

    Implemented NotAfterCondition.

---
+++ src/Pure.Primitives.Time.Operations/NotAfterCondition.cs
@@ -0,0 +1,55 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.Time;
+
+namespace Pure.Primitives.Time.Operations;
+
+public sealed record NotAfterCondition : IBool
+{
+    private readonly IEnumerable<ITime> _values;
+
+    public NotAfterCondition(params ITime[] values) : this(values.AsReadOnly()) { }
+
+    public NotAfterCondition(IEnumerable<ITime> values)
+    {
+        _values = values;
+    }
+
+    bool IBool.BoolValue
+    {
+        get
+        {
+            using IEnumerator<ITime> enumerator = _values.GetEnumerator();
+
+            if (!enumerator.MoveNext())
+            {
+                throw new ArgumentException();
+            }
+
+            IComparer<ITime> comparer = new TimeComparer();
+
+            ITime previous = enumerator.Current;
+
+            while (enumerator.MoveNext())
+            {
+                if (comparer.Compare(previous, enumerator.Current) < 0)
+                {
+                    return false;
+                }
+
+                previous = enumerator.Current;
+            }
+
+            return true;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
