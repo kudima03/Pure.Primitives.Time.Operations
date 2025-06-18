01a10d3dded8ca568a3c499e43462a80ce3e4d25
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-18 15:20:02 +0300

    Added tests for EqualCondition.

---
+++ src/Tests/Pure.Primitives.Time.Operations.Tests/EqualConditionTests.cs
@@ -0,0 +1,79 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Number;
+using Pure.Primitives.Random.Time;
+
+namespace Pure.Primitives.Time.Operations.Tests;
+
+public sealed record EqualConditionTests
+{
+    [Fact]
+    public void TakesPositiveResultOnSameValues()
+    {
+        IBool equality = new EqualCondition(
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))));
+
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesPositiveResultOnTwoSameValues()
+    {
+        IBool equality = new EqualCondition(
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))));
+
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesNegativeResultOnDifferentValues()
+    {
+        IBool equality = new EqualCondition(new RandomTimeCollection(new UShort(10)));
+
+        Assert.False(equality.BoolValue);
+    }
+
+    [Fact]
+    public void TakesNegativeResultOnAllSameOneDifferentValue()
+    {
+        IBool equality = new EqualCondition(
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new Time(TimeOnly.FromTimeSpan(new TimeSpan(1, 1, 1))),
+            new RandomTime());
+
+        Assert.False(equality.BoolValue);
+    }
+
+    [Fact]
+    public void ProduceTrueOnSingleElementInCollection()
+    {
+        IBool equality = new EqualCondition(new CurrentTime());
+        Assert.True(equality.BoolValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnEmptyArguments()
+    {
+        IBool equality = new EqualCondition();
+        Assert.Throws<ArgumentException>(() => equality.BoolValue);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(()=> new EqualCondition(new RandomTime()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(()=> new EqualCondition(new RandomTime()).ToString());
+    }
+}
